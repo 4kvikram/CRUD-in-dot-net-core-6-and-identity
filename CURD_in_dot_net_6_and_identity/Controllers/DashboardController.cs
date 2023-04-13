379d79c45b54d6b7b54fc3b8fe36e7604fec52c0
Vikram <4kviky@gmail.com>
2023-04-13 11:14:12 +0530

    added employee listing

--- CURD_in_dot_net_6_and_identity/Controllers/DashboardController.cs
+++ CURD_in_dot_net_6_and_identity/Controllers/DashboardController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CURD_in_dot_net_6_and_identity.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CURD_in_dot_net_6_and_identity.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly ApplicationContext _context;
+
+        public DashboardController(ApplicationContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var data = _context.Employee.ToList();
+            return View(data);
         }
     }
 }
